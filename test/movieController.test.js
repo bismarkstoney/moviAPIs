0a078b7c9d626764d1f7289324ddcc56fa9369e9
bismarkstoney <bismarkstoney@yahoo.com>
2022-10-15 11:49:34 +0000

    change deployment to heroku

--- test/movieController.test.js
+++ test/movieController.test.js
@@ -2,7 +2,6 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../server');
 
-//Assertion
 chai.should();
 chai.use(chaiHttp);
 describe('Movie APIs', () => {
